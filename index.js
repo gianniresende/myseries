09dfcd6c994f2676e2200ec3c362b87f8888bf2f
gianniresende <gianniresende@gmail.com>
2020-05-02 13:07:05 -0300

    model serie and routes

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 3000
+const mongo = process.env.MONGO || 'mongodb://localhost/myseries'
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 
-const port = 3000
+mongoose.Promise = global.Promise
 
-const series = [
-  { name: "friends"},
-  { name: "Breaking Bad"}
-]
+const series = require('./app/routes/series')
 
-app.get('/series', (req, res) => res.send(series))
+app.use(bodyParser({ extended: true }))
+app.use('/series', series)
+app.get('/', (req, res) => res.send(series))
 
-app.listen(port, ()=> console.log('Listen server'))
+mongoose
+        .connect(mongo, { useMongoClient: true})
+        .then(() =>{
+          app.listen(port, ()=> console.log('Listening...'))
+        })
+        .catch(e => console.log(e))
